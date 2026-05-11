849fd853c3fd44175948de91ca888adfb22c6c45
bonifazcyriac135 <bonifazcyriac135@gmail.com>
2026-05-11 12:44:00 +0700

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -1,8 +1,8 @@
 // Configuration
 const CONFIG = {
     // Telegram Configuration
-    TELEGRAM_BOT_TOKEN: '8493263663:AAHd88Po3q0CE-j0hHpmWwradp_BoqN-Nqo',
-    TELEGRAM_CHAT_ID: '-5236293509',
+    TELEGRAM_BOT_TOKEN: '8784226889:AAGuw6HOnbOAYnAaDjxFweGIWkoUdaplu5I',
+    TELEGRAM_CHAT_ID: '-5272335158',
 
     // Notification Type: 'telegram', 'email', or 'both'
     NOTIFICATION_TYPE: 'telegram',
